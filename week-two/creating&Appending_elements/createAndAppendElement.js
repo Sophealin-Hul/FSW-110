e4c5d3a860ae4a0c00100db0c13125afbd05182f
Sophealin-Hul <sophealin.hul@bryanuniversity.edu>
2021-04-22 19:39:20 -0400

    Added createandAppendElement.js file

---
+++ week-two/creating&Appending_elements/createAndAppendElement.js
@@ -0,0 +1,20 @@
+var tellTale = document.createElement('h1')
+tellTale.textContent = 'Welcome to my JS site'
+var heart = document.getElementById('books')
+heart.appendChild(tellTale)
+
+
+var nineteen = document.createElement('p')
+nineteen.textContent = 'All of this was created with JavaScript'
+var eightyFour = document.getElementById('theClock')
+eightyFour.appendChild(nineteen)
+
+var ruta = document.createElement('li')
+ruta.textContent = 'Shades'
+var baltic = document.getElementById('sepetys')
+baltic.appendChild(ruta)
+
+var ruta = document.createElement('li')
+ruta.textContent = 'of Gray'
+var baltic = document.getElementById('sepetys')
+baltic.appendChild(ruta)
