967300304525de64607829c1213b93b8b9892044
Sophealin-Hul <sophealin.hul@bryanuniversity.edu>
2021-05-16 12:32:42 -0400

    modified index.js

--- week5/submit_events2/index.js
+++ week5/submit_events2/index.js
@@ -4,10 +4,15 @@ function showAlert() {
     var aged = document.getElementById('age').value;
     var selected = document.querySelector('input[type=radio][name=decision]:checked').value;
     var rates = document.getElementById('rate').value;
-    var purchase = document.querySelector('input[type=checkbox][name=items]:checked').value;
-    
+    var purchase = document.getElementsByName('items');
+    var selectedPurchcases = '';
+    for (var i=0; i<purchase.length; i++) {
+        if (purchase[i].type == 'checkbox' && purchase[i].checked == true )
+            selectedPurchcases += purchase[i].value + ' ' + ' ';
+    }
+
     alert('First Name: ' + fName + '\n' + 'Last Name: ' + lName + '\n' + 'Age: ' + aged + '\n' + 'Member: '
-            + selected + '\n' + 'Rate Our Store: ' + rates + '\n' + 'Purchased: ' + purchase);
+            + selected + '\n' + 'Rate Our Store: ' + rates + '\n' + 'Purchased: ' + selectedPurchcases);
     
 
 }
