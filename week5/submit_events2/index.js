393c8722d222b5544a583499f509f21a65578ca8
Sophealin-Hul <sophealin.hul@bryanuniversity.edu>
2021-05-16 11:56:27 -0400

    added two files to submit_events2

--- week5/submit_events2/index.js
+++ week5/submit_events2/index.js
@@ -1,14 +1,13 @@
 function showAlert() {
-    var fname = document.getElementById('firstName').value;
-    var lname = document.getElementById('lastName').value;
-    var age = document.getElementById('age').value;
-    var deci1 = document.getElementById('decision1').value;
-    var deci2 = document.getElementById('decision2').value;
-    var rating = document.getElementById('rate').value;
-    var purchase1 = document.getElementById('item1').value;
-    var purchase2 = document.getElementById('item2').value;
-    var purchase3 = document.getElementById('item3').value;
-    alert('First Name: ' +fname + '\n' + 'Last Name: ' + lname + '\n' + 'Age: ' + age + '\n' + deci1 + 
-            '\n' + deci2 + '\n' + rating + '\n' + purchase1 + '\n' + purchase2 + '\n' + purchase3);
-    return false;
+    var fName = document.getElementById('firstName').value;
+    var lName = document.getElementById('lastName').value;
+    var aged = document.getElementById('age').value;
+    var selected = document.querySelector('input[type=radio][name=decision]:checked').value;
+    var rates = document.getElementById('rate').value;
+    var purchase = document.querySelector('input[type=checkbox][name=items]:checked').value;
+    
+    alert('First Name: ' + fName + '\n' + 'Last Name: ' + lName + '\n' + 'Age: ' + aged + '\n' + 'Member: '
+            + selected + '\n' + 'Rate Our Store: ' + rates + '\n' + 'Purchased: ' + purchase);
+    
+
 }
